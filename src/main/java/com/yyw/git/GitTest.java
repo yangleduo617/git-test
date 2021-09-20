ed1b941d8e6b9fb2f5b40bfd00976a4cfbfe3f6e
Yang <yywqdu@163.com>
2021-09-20 16:18:04 +0800

    hot-fix1 commit

--- src/main/java/com/yyw/git/GitTest.java
+++ src/main/java/com/yyw/git/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("hello Git");
         System.out.println("hello GitHub");
         System.out.println("hello Gitee");
+        System.out.println("hello GitLab");
     }
 }
