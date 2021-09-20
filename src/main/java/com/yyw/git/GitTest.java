054a1664a06c6d7863ea8bc151f04313d9354654
Yang <yywqdu@163.com>
2021-09-20 15:55:34 +0800

    third commit

--- src/main/java/com/yyw/git/GitTest.java
+++ src/main/java/com/yyw/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello Git");
         System.out.println("hello GitHub");
+        System.out.println("hello Gitee");
     }
 }
